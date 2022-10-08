e31aed427234ebb7f6a059baf71a9acb1cf45f19
Olixpin <noibiabdulrasaq@gmail.com>
2022-10-08 03:49:12 +0100

    navbar styling done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,6 +1,182 @@
+import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        width="100%"
+        alt=""
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint itaque
+        beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        itaque beatae dolore soluta a eius voluptatibus non! Quo corrupti facere
+        aperiam quis quidem, itaque sunt quod autem eveniet! Reprehenderit,
+        nihil.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
