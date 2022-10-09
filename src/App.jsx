95b603cfa3d86f6e2455da5bff53d89d3a1b5994
Olixpin <noibiabdulrasaq@gmail.com>
2022-10-09 03:24:03 +0100

    completed the first version of the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import ErrorBoundary from "./ErrorBoundary";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <ErrorBoundary>
-        {/* <Home /> */}
-        {/* <Watch /> */}
+        <Home />
+        <Watch />
         <Register />
+        <Login />
       </ErrorBoundary>
       {/* video */}
     </div>
