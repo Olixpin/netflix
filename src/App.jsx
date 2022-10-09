f732b8fcea644fbe9f1e2c2cdc32240fe00c2422
Olixpin <noibiabdulrasaq@gmail.com>
2022-10-09 01:21:18 +0100

    Watch Page Version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import ErrorBoundary from "./ErrorBoundary";
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 
 const App = () => {
   return (
     <div>
       <ErrorBoundary>
-        <Home />
+        {/* <Home /> */}
+        <Watch />
       </ErrorBoundary>
       {/* video */}
     </div>
---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,23 @@
+import { ArrowBackOutlined } from "@material-ui/icons";
+import "./watch.scss";
+
+const Watch = () => {
+  return (
+    <div className="watch">
+      <div className="back">
+        <ArrowBackOutlined />
+        Home
+      </div>
+      <video
+        controls
+        progress
+        autoPlay={true}
+        loop
+        src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+        poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
+      ></video>
+    </div>
+  );
+};
+
+export default Watch;
